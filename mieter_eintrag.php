d3f68a29f83f4746ffbc01c4dfd17a6466e31794
raebe <raebe@DESKTOP-4Q10Q7I>
2019-10-13 16:40:39 +0200

    update queres

--- mieter_eintrag.php
+++ mieter_eintrag.php
@@ -4,26 +4,26 @@
         <title></title>
     </head>
     <body>
-       
+
         <?php
         $anrede = $_GET['anrede'];
         $vorname = $_GET['vorname'];
         $name = $_GET['name'];
         $geburtsdatum = $_GET['geburtsdatum'];
-
-
+  
+      
         // Datenbankangaben sollten in ein db.inc.php geschrieben werden
         include 'db.inc.php';
-        
-        $link=mysqli_connect("localhost", $benutzer, $passwort) or die("Keine Verbindung zur Datenbank!");
+
+        $link = mysqli_connect("localhost", $benutzer, $passwort) or die("Keine Verbindung zur Datenbank!");
         mysqli_select_db($link, $dbname) or die("DB nicht gefunden");
-        
-        $insert="INSERT INTO `mieter` (`ID`, `anrede`, `vorname`, `name`, `geburtsdatum`) "
+
+        $insert = "INSERT INTO `mieter` (`mieterID`,`anrede`, `vorname`, `name`, `geburtsdatum`) "
                 . "VALUES (NULL,'$anrede', '$vorname', '$name', '$geburtsdatum')";
-        
-        mysqli_query($link,"SET NAMES 'utf8'");
+
+        mysqli_query($link, "SET NAMES 'utf8'");
         mysqli_query($link, $insert) or die("Eintrag hat nicht geklappt");
-        
+
         mysqli_close($link);
         echo "Der neue Mieter wurde erfasst!";
         ?>
