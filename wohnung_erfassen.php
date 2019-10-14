7822bdf7972378fa9c2c44ce54af52c459db75cd
Sebrina <Sebrina@192.168.0.28>
2019-10-14 13:13:29 +0200

    Haus erfassen gefixt mit neuen DB-Feldern, Wohnung erfassen geht noch nicht

--- wohnung_erfassen.php
+++ wohnung_erfassen.php
@@ -13,22 +13,18 @@
     <body>
         <h3>Bitte tragen Sie die Angaben zur Wohnung ein:</h3>
         <form action="wohnung_eintrag.php" method="GET">
-            Strasse, Nr. <br/><input type="text" name="strasse_nr" value="" /><br/>
-            PLZ <br/><input type="text" name="plz" value="" /><br/>
-            Ort <br/><input type="text" name="ort" value="" /><br/>
-            <p>
             Anzahl Zimmer<br/> <input type="text" name="zimmer" value="" /><br/>
             Fläche in qm<br/><input type="text" name="flaeche" value="" /><br/>
             
             <p>
-            Haus <br/><select name="haus">
+            Haus <br/><select name="hausID">
                 
             <?php
             $res=mysqli_query($link, "SELECT * FROM haus");
             while($row=mysqli_fetch_array($res))
                 {
                 ?>
-                 <option> <?php echo $row["bezeichnung"] ?></option>
+                 <option value =<?php echo $row["hausID"]?>> <?php echo $row["bezeichnung"] ?></option>
                     <?php
                 }
                 ?>
@@ -38,7 +34,7 @@
             <input type="submit" value="eintragen" /><input type="reset" value="Formular leeren" />
         </form>
         
-        <a href="mieter_ausgabe.php">Erfasste Wohnungen darstellen</a><br/>
+        <a href="wohnung_ausgabe.php">Erfasste Wohnungen darstellen</a><br/>
         <a href="index.php">Startseite</a><br/>
         
         
