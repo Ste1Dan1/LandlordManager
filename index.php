84b22e1373e8e5a93e343b5d84165a8859fbfff1
raebe <raebe@DESKTOP-4Q10Q7I>
2019-10-18 21:33:58 +0200

    mieter CRUD

--- index.php
+++ index.php
@@ -16,14 +16,9 @@ Startseite mit Menüleiste und klickbaren Icons
         
         <h1>Herzlich Willkommen bei Landlord Manager</h1>
         <h2>Hier finden Sie alles was Sie für Ihre Tätigkeit als Vermieter brauchen!</h2>
-        <?php
+
         
-        session_start();
-        include 'loginCheck.inc.php';
-         
-        ?>
-        
-        <a href="mieter_ausgabe.php"><img src="images/Mieter.png" alt="Mieter-Icon" height="200" width="200"</a>
+        <a href="mieter.php"><img src="images/Mieter.png" alt="Mieter-Icon" height="200" width="200"</a>
         <a href="wohnung_ausgabe.php"><img src="images/immobilien.png" alt="Immobilien-Icon" height="200" width="200"</a>
         <a href="nkrechnungen_ausgabe.php"><img src="images/Nebenkosten.png" alt="Nebenkosten-Icon" height="200" width="200"</a>
         <a href=#reporting><img src="images/Reporting.png" alt="Reporting-Icon" height="200" width="200"</a>
