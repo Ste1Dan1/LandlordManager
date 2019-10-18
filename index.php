5e10ac754fb85274de06e59410865db61777988f
Sebrina <Sebrina@192.168.0.28>
2019-10-18 14:49:39 +0200

    Neue Startseiten-Icons

--- index.php
+++ index.php
@@ -26,6 +26,7 @@ Startseite mit Menüleiste und klickbaren Icons
         <a href="mieter_ausgabe.php"><img src="images/Mieter.png" alt="Mieter-Icon" height="200" width="200"</a>
         <a href="wohnung_ausgabe.php"><img src="images/immobilien.png" alt="Immobilien-Icon" height="200" width="200"</a>
         <a href="nkrechnungen_ausgabe.php"><img src="images/Nebenkosten.png" alt="Nebenkosten-Icon" height="200" width="200"</a>
+        <a href=#reporting><img src="images/Reporting.png" alt="Reporting-Icon" height="200" width="200"</a>
 
         
     </body>
