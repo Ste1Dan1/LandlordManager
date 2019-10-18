8acefd67adb81360713811a7882165f93f6ef0f5
raebe <raebe@DESKTOP-4Q10Q7I>
2019-10-18 21:34:14 +0200

    mieter CRUD

--- db.inc.php
+++ db.inc.php
@@ -2,5 +2,7 @@
 $benutzer="landlord_DB_user";
 $passwort="";
 $dbname="landlordmanager";
+        $link=mysqli_connect("localhost", $benutzer, $passwort, $dbname) or die("Keine Verbindung zur Datenbank!");
+        mysqli_select_db($link, $dbname) or die("DB nicht gefunden");
 ?>
 
