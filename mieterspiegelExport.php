3e6c26b1c828d92f8e790b0ea4013c7c59de6b5c
raebe <raebe@DESKTOP-4Q10Q7I>
2019-11-04 23:28:57 +0100

    Topbar & Mieterspiegel Alte Files gelöscht

--- mieterspiegelExport.php
+++ mieterspiegelExport.php
@@ -1,4 +1,5 @@
 <?php
+
 //include database configuration file
 include 'db.inc.php';
 
@@ -11,34 +12,32 @@ FROM `mietvertrag`
         WHERE cast(mietende as date) >=  cast(CURDATE() as date) 
         OR mietende IS NULL");
 
-if($query->num_rows > 0){
-    $delimiter = ",";
+if ($query->num_rows > 0) {
+    $delimiter = ',';
     $filename = "mieterspiegel_" . date('Y-m-d') . ".csv";
-    
     //create a file pointer
     $f = fopen('php://memory', 'w');
-    
     //set column headers
-    $fields = array("Anrede", "Vorname", "Name", "Strasse Nr", "PLZ", "Ort", "Gebäude", "Wohnungs-Nr.", "Mietbeginn", "Mietende", "Mietzins", "Nebenkosten");
+    $fields = array("Anrede", "Vorname", "Name", "StrasseNr", "PLZ", "Ort", "Gebäude", "WohnungsNr", "Mietbeginn", "Mietende", "Mietzins", "Nebenkosten");
     fputcsv($f, $fields, $delimiter);
-    
+
     //output each row of the data, format line as csv and write to file pointer
-    while($row = $query->fetch_assoc()){
-        $lineData = array($row['anrede'],$row['vorname'], $row['name'], $row['strasse_nr'], $row['plz'], $row['ort'], $row['bezeichnung'], $row['wohnungsNummer'], $row['mietbeginn'], $row['mietende'], $row['mietzins_mtl'],$row['nebenkosten_mtl']);
+    while ($row = $query->fetch_assoc()) {
+        $lineData = array($row['anrede'], $row['vorname'], $row['name'], $row['strasse_nr'], $row['plz'], $row['ort'], $row['bezeichnung'], $row['wohnungsNummer'], $row['mietbeginn'], $row['mietende'], $row['mietzins_mtl'], $row['nebenkosten_mtl']);
         fputcsv($f, $lineData, $delimiter);
-    }
-    
-     
+    }   
+
+
     //move back to beginning of file
     fseek($f, 0);
-    
+
     //set headers to download file rather than displayed
     header('Content-Type: application/csv');
     header('Content-Disposition: attachment; filename="' . $filename . '";');
-    
+
     //output all remaining data on a file pointer
+    ob_end_clean();
     fpassthru($f);
 }
 exit;
-
 ?>
